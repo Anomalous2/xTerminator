0f1ab2d72ee41ff6a82b3e2fa97b5727ea7308ca
Anomalous2 <44941665+Anomalous2@users.noreply.github.com>
2018-11-12 11:07:09 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Home</title>
+
+</head>
+<body>
+
+<?php
+include "header.html"
+
+?>
+
+<div class="container">
+    <div class="jumbotron">
+        <h1>xTerminator</h1>
+        <br>
+
+        <div class="row">
+            <div class="col-lg-12 col-md-12">
+                <h3>sem daj popis aplikacie</h3>
+                <p>V následujícím článku můžete zhlédnout videa, která se vážou k novým rasám. Některá se vám přehrají poté, co si vytvoříte postavu nové rasy a lognete do hry. Jiná poslouží jako krátké cinematicy v průběhu questlinií, v rámci nichž zrekrutujete novou rasu.</p>
+                <p class="float-right citajviac" data-toggle="collapse" data-target="#cl2">Čítaj viac...</p>
+                <div id="cl2" class="collapse">
+                    <br>
+                    <p>Už jste pořádně nahypovaní na nové rasy? To jo dobře, tentokrát tu tutiž máme něco lepšího než jen modely a texty. A to dokonce přímo videa. Jde o cutscény ze hry. Některé se vám přehrají poté, co si vytvoříte postavu nové rasy a lognete do hry (zkrátka úvodní videa, jaká má každá rasa), jiné poslouží jako krátké cinematicy v průběhu questových linií. Tyto questové linie budou popisovat, proč a za jakých okolností se daná rasa spojí s Aliancí nebo Hordou.
+                        Všechny články o nových rasách najdete pod klíčovými slovy nové rasy, popř. datamining.
+                        Související článek: dialogy ohledně přijetí nových ras
+                        Videa obsahují příběhové spoilery. Zároveň se nedá vyloučit, že tento obsah ještě projde změnami, než se dostane na live servery.
+                        Pod každým videem najdete ve spoileru přepis dialogů přeložený do češtiny.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ nahlasitVyskyt.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Home</title>
+
+</head>
+<body>
+
+<?php
+include "header.html"
+
+?>
+
+<div class="container">
+    <div class="jumbotron">
+        <h1>Nahlasit vyskyt</h1>
+        <br>
+
+
+    </div>
+</div>
